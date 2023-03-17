c875ff1cf54230e81118907bb94e7619e4132eba
YESHWANTH RAGHAVENDAR R <113741529+Yeshu-18@users.noreply.github.com>
2023-03-17 22:09:24 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+// Select the button element using the document.querySelector method
+const button = document.querySelector("button");
+
+// Add a click event listener to the button element
+button.addEventListener("click", function() {
+  // Set the background color of the body element to blue
+  document.body.style.backgroundColor = "blue";
